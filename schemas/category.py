7e1dbc403ddb3bc67547f110afe5abc1626318d7
Got It <gotit@Fahads-MBP-2.localdomain>
2019-05-29 18:30:31 +0700

    implement models and schemas

--- schemas/category.py
+++ schemas/category.py
@@ -1,6 +1,8 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, validate
 
 
 class CategorySchema(Schema):
-    name = fields.Str(required=True)
-
+    name = fields.Str(required=True,
+                      validate=validate.Length(min=6,
+                                               max=30,
+                                               error='Category name must contain 6 to 30 characters.'))
