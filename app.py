782612c30ba0417fa35ed651e3d70134751f4983
Got It <gotit@Fahads-MBP-2.localdomain>
2019-06-06 12:15:55 +0700

    change the way that application loads config, add different configs for different environments

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
+import os
+
 from flask import Flask
 from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
-app.config.from_object('config')
-jwt = JWTManager(app)
 
+ENV = os.environ.get('ENV')
+if not ENV:
+    ENV = 'development'
+if ENV == 'development':
+    app.config.from_object('config.DevelopmentConfig')
+elif ENV == 'production':
+    app.config.from_object('config.ProductionConfig')
+jwt = JWTManager(app)
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 from controllers import *
-from models import *
--- config.py
+++ config.py
@@ -1,6 +1,23 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE', 'mysql://root:281096^^@localhost/item_management')
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = 'hodns'
-PER_PAGE = 3
+
+class Config(object):
+    DEBUG = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_ACCESS_TOKEN_EXPIRES = 900
+    NUMBER_OF_RECORDS_IN_ONE_PAGE = 10
+    # SECRET_KEY = os.urandom(24)
+    SECRET_KEY = 'w6kt4g32y3'
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE', 'mysql://root:281096^^@localhost/item_management')
+    ENV = 'production'
+
+
+class DevelopmentConfig(Config):
+    JWT_ACCESS_TOKEN_EXPIRES = 3600
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE', 'mysql://root:281096^^@localhost/item_management')
+    NUMBER_OF_RECORDS_IN_ONE_PAGE = 3
+    ENV = 'development'
+    DEBUG = True
