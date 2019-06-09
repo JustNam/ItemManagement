fc8664ce477b92b2c5d2e0b0164fd7f25b8382ae
Got It <justn4m@gmail.com>
2019-06-09 07:11:57 +0700

    edit config example

--- config.example.py
+++ config.example.py
@@ -16,7 +16,7 @@ class ProductionConfig(Config):
 
 class DevelopmentConfig(Config):
     JWT_ACCESS_TOKEN_EXPIRES = 3600
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE', 'mysql://user:281096^^@localhost/database')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE', 'mysql://user:password@localhost/database')
     ITEMS_PER_PAGE = 3
     ENV = 'development'
     DEBUG = True
